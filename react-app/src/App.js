3a6b378d6b44bc55e7130cc2eaabd9bd291eee7f
johnnyCake1 <zheenbek.akimzhanov@gmail.com>
2024-06-10 13:36:46 +0600

    update react-app

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Wassapaa
+          React App v1
         </p>
         <a
           className="App-link"
